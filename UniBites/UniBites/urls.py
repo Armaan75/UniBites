5f1724583f59254a2f9060e069e73da2a836baf0
Armaan75 <ibitefifa@gmail.com>
2023-08-19 00:01:36 +0500

    Migrations done with creating a superuser

--- UniBites/UniBites/urls.py
+++ UniBites/UniBites/urls.py
@@ -22,6 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('accounts.urls')),
-    # path('', include('App.urls')),
+    path('', include('App.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
