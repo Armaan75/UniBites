23729531aab0af9ef3a208072bae353ff1b13e5f
Armaan75 <ibitefifa@gmail.com>
2023-08-18 23:42:06 +0500

    model of Profile Image is added

--- UniBites/accounts/models.py
+++ UniBites/accounts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    profile_image = models.ImageField(upload_to = 'images', null=True)
+    
