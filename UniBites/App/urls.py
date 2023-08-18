7be21eac4a2057c8b52bc23e8446d33dfe6c9c01
Armaan75 <ibitefifa@gmail.com>
2023-08-19 00:15:51 +0500

    urls have been updated

--- UniBites/App/urls.py
+++ UniBites/App/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import home
+from .views import home, cafe_list, food_list, checkout
 
 app_name = 'app'
 urlpatterns = [
     path('', home, name='home'),
+    path('cafe', cafe_list, name='cafe_list'),
+    path('cafe/<int:cafe_id>/', food_list, name='food_list'),
+    path('cafe/<int:cafe_id>/checkout/', checkout, name='checkout'),
 
 ]
