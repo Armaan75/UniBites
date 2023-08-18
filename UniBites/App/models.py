e1c38ce91723c1f2c62b3a5594154b17464e32bc
Armaan75 <ibitefifa@gmail.com>
2023-08-19 00:11:16 +0500

    the models of Cafe FoodItem and Order have been created

--- UniBites/App/models.py
+++ UniBites/App/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+class Cafe(models.Model):
+    name = models.CharField(max_length=100)
+
+class FoodItem(models.Model):
+    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
+    items = models.ManyToManyField(FoodItem)
+    note = models.TextField(blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
