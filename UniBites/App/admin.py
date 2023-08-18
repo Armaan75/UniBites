1a738630b3e68e7de15a5edc1f2dcd397cb3945d
Armaan75 <ibitefifa@gmail.com>
2023-08-19 00:27:50 +0500

    templates are updated

--- UniBites/App/admin.py
+++ UniBites/App/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Cafe, FoodItem, Order
 
-# Register your models here.
+@admin.register(Cafe)
+class CafeAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(FoodItem)
+class FoodItemAdmin(admin.ModelAdmin):
+    list_display = ('name', 'cafe', 'price')
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('user', 'cafe', 'timestamp')
