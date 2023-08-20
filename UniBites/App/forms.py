64d2c0b844a7394b860082824b5dcc3626c61588
Armaan75 <ibitefifa@gmail.com>
2023-08-21 01:39:35 +0500

    models are set

--- UniBites/App/forms.py
+++ UniBites/App/forms.py
@@ -1,6 +1,13 @@
 from django import forms
 
 class PaymentForm(forms.Form):
-    card_number = forms.CharField(max_length=16, label='Card Number', widget=forms.TextInput(attrs={'placeholder': '1234 5678 9012 3456'}))
-    expiration_date = forms.DateField(label='Expiration Date', error_messages={}, widget=forms.TextInput(attrs={'placeholder': 'MM/YY'}))
-    cvv = forms.CharField(max_length=3, label='CVV', error_messages={}, widget=forms.TextInput(attrs={'placeholder': '123'}))
+    card_number = forms.CharField(max_length=16, label='Card Number',  widget=forms.TextInput(attrs={'placeholder': '1234 5678 9012 3456'}))
+    expiration_date = forms.DateField(label='Expiration Date',   widget=forms.TextInput(attrs={'placeholder': 'MM/YY'}))
+    cvv = forms.CharField(max_length=3, label='CVV',   widget=forms.TextInput(attrs={'placeholder': '123'}))
+
+
+    def __init__(self, *args, **kwargs):
+        super(PaymentForm, self).__init__(*args, **kwargs)
+        self.fields['card_number'].help_text = ''
+        self.fields['expiration_date'].help_text = ''
+        self.fields['cvv'].help_text = ''
--- UniBites/App/models.py
+++ UniBites/App/models.py
@@ -23,8 +23,6 @@ class Order(models.Model):
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # Add this field
     timestamp = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        return self.user
 
 
 class Payment(models.Model):
