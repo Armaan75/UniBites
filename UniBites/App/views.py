84da8bf016715b578c7c7cd361484baef96f1fc8
Armaan75 <ibitefifa@gmail.com>
2023-08-21 00:31:44 +0500

    views updated

--- UniBites/App/views.py
+++ UniBites/App/views.py
@@ -1,14 +1,12 @@
-from django.shortcuts import render
-from .models import Cafe, FoodItem, Order, Payment
+# views.py
+
 from django.shortcuts import render, redirect
 from django.contrib import messages
-
+from .models import Cafe, FoodItem, Order, Payment
 from .forms import PaymentForm
-
 from django.core.mail import send_mail
 from django.conf import settings
 
-# Create your views here.
 def home(request):
     return render(request, 'app/home.html')
 
@@ -21,18 +19,16 @@ def food_list(request, cafe_id):
     food_items = FoodItem.objects.filter(cafe=cafe)
     return render(request, 'app/food_list.html', {'cafe': cafe, 'food_items': food_items})
 
-
-
-
-
 def checkout(request, cafe_id):
     if request.method == 'POST':
         selected_food_ids = request.POST.getlist('food')
         note = request.POST.get('note', '')
         cafe = Cafe.objects.get(pk=cafe_id)
         selected_food_items = FoodItem.objects.filter(pk__in=selected_food_ids)
-        total_price = sum(item.price for item in selected_food_items)
         
+        # Calculate the total price
+        total_price = sum(item.price for item in selected_food_items)
+
         order = Order.objects.create(
             user=request.user,  
             cafe=cafe,
@@ -40,6 +36,7 @@ def checkout(request, cafe_id):
         )
         order.items.set(selected_food_items)
 
+        # Send the order confirmation email
         subject = 'Order Confirmation'
         message = f'Hello {request.user.username}, your order has been placed successfully.\n\nOrder details:\nCafe: {cafe.name}\nTotal Price: ${total_price}\n\nThank you for your order!'
         from_email = settings.DEFAULT_FROM_EMAIL
@@ -47,34 +44,17 @@ def checkout(request, cafe_id):
         
         send_mail(subject, message, from_email, recipient_list, fail_silently=True)
 
-        return redirect('app:order_confirmation', order_id=order.id)
+        return render(request, 'app/order_confirmation.html', {'user': request.user, 'order': order, 'total_price': total_price})
     else:
         return redirect('app:cafe_list')
 
-
-
-
-def order_confirmation(request, order_id):
-    try:
-        order = Order.objects.get(pk=order_id)
-    except Order.DoesNotExist:
-        return redirect('app:cafe_list')  
-
-
-    subject = 'Order Confirmation'
-    message = f'Hello {order.user.username}, your order has been placed successfully.\n\nOrder details:\nCafe: {order.cafe.name}\nTotal Price: ${order.total_price}\n\nThank you for your order!'
-    from_email = settings.DEFAULT_FROM_EMAIL
-    recipient_list = [order.user.email]
-
-    send_mail(subject, message, from_email, recipient_list, fail_silently=True)
-
-    return render(request, 'app/order_confirmation.html', {'order': order})
-
-
-
 def make_payment(request, order_id):
     order = Order.objects.get(pk=order_id)
-    
+    selected_food_ids = request.POST.getlist('food')
+    note = request.POST.get('note', '')
+
+    selected_food_items = FoodItem.objects.filter(pk__in=selected_food_ids)
+    total_price = sum(item.price for item in selected_food_items)
 
     if request.method == 'POST':
         form = PaymentForm(request.POST)
@@ -95,4 +75,21 @@ def make_payment(request, order_id):
     else:
         form = PaymentForm()
 
-    return render(request, 'app/payment.html', {'form': form, 'order': order})
+    return render(request, 'app/payment.html', {'form': form, 'order': order, 'total_price': total_price})
+
+def order_confirmation(request, order_id):
+    order = Order.objects.get(pk=order_id)
+    selected_food_ids = request.POST.getlist('food')
+    note = request.POST.get('note', '')
+
+    selected_food_items = FoodItem.objects.filter(pk__in=selected_food_ids)
+    total_price = sum(item.price for item in selected_food_items)
+
+    subject = 'Order Confirmation'
+    message = f'Hello {order.user.username}, your order has been placed successfully.\n\nOrder details:\nCafe: {order.cafe.name}\nTotal Price: ${order.total_price}\n\nThank you for your order!'
+    from_email = settings.DEFAULT_FROM_EMAIL
+    recipient_list = [order.user.email]
+
+    send_mail(subject, message, from_email, recipient_list, fail_silently=True)
+
+    return render(request, 'app/order_confirmation.html', {'order': order, 'total_price': total_price})
