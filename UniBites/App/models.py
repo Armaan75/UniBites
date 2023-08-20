ed30443c27a26f3b8eb4a4579ac3e23cfd2c05a2
Armaan75 <ibitefifa@gmail.com>
2023-08-20 12:52:01 +0500

    models updated

--- UniBites/App/models.py
+++ UniBites/App/models.py
@@ -4,10 +4,16 @@ from accounts.models import User
 class Cafe(models.Model):
     name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
 class FoodItem(models.Model):
     cafe = models.ForeignKey(Cafe, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -16,3 +22,6 @@ class Order(models.Model):
     note = models.TextField(blank=True)
     total_price = models.DecimalField(max_digits=10, decimal_places=2, default=0)  # Add this field
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user
